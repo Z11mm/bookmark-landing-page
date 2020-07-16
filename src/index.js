b1981d34e80e2107473180b4fce13205f1583607
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-07-16 10:25:32 +0100

    Style: Set up node sass
    Add sass script and directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './main.scss';
+import './assets/css/main.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
