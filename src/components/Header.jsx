a276105dbed60ef8a5efb09e48cca8fccfd2ed78
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-07-17 21:35:45 +0100

    Add header component
    Refactor App component and add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import brandlogo from '../assets/images/logo-bookmark.svg'
+
+const Header = () => (
+  <header>
+    <div>
+      <img src={brandlogo} alt='' />
+    </div>
+  </header>
+);
+
+export default Header;
