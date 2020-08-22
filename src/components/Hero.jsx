2e94516f2b03392c536f6c73d9f99bda565019bc
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-08-22 18:41:48 +0100

    Add content to hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+
+import heroImage from '../assets/images/illustration-hero.svg'
 
 const Hero = () => (
   <div>
-    <h1>A Simple Bookmark Manager</h1>
+    <div>
+      <h1>A Simple Bookmark Manager</h1>
+
+      <p>
+        A clean and simple interface to organize your favorite websites. Open a
+        new browser tab and see your sites load instantly. Try it for free.
+      </p>
+
+      <div>
+        <button>Get it on Chrome</button>
+        <button>Get it on Firefox</button>
+      </div>
+    </div>
+
+    <div>
+      <img
+        src={heroImage}
+        alt='An illustration of a device page'
+      />
+    </div>
   </div>
 );
 
