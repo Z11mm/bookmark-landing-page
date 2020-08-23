1fe306d9b5b4b6ee42f34277d185a7669cee1e85
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-08-23 20:09:25 +0100

    Markup: Add features component to App component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
-import Header from "./components/Header";
-import Hero from './components/Hero'
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Features from './components/Features';
 
 import './assets/css/main.css';
 
 const App = () => {
   return (
-    <main className="App">
+    <main className='App'>
       <Header />
       <Hero />
+      <Features />
     </main>
   );
-}
+};
 
 export default App;
--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
 const Features = () => (
-  <div></div>
-)
+  <div>
+    <section>
+      <h2>Features</h2>
+      <p>
+        Our aim is to make it quick and easy for you to access your favourite
+        websites. Your bookmarks sync between your devices so you can access
+        them on the go.
+      </p>
+    </section>
+  </div>
+);
+
+export default Features;
