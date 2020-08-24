e9f139521cde85b987ea7a0f17ff556c22b48e18
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-08-24 20:41:10 +0100

    Markup: Create extensions component and add contents

---
+++ src/components/Extensions.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Extensions = () => (
+  <div>
+    <section>
+      <h2>Download the extension</h2>
+      <p>
+        We’ve got more browsers in the pipeline. Please do let us know if you’ve
+        got a favourite you’d like us to prioritize.
+      </p>
+    </section>
+  </div>
+);
