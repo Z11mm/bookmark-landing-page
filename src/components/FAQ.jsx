2af2854b9db9106263bce90b7e6605d5f1d9f03b
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-08-26 19:55:14 +0100

    Markup: Add content to FAQ component

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const FAQ = () => ()
+const FAQ = () => (
+  <section>
+    <h2>Frequently Asked Questions</h2>
+    <p>
+      Here are some of our FAQs. If you have any other questions you’d like
+      answered please feel free to email us.
+    </p>
+  </section>
+);
 
 export default FAQ
