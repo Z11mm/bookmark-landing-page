07bcdc468f90c1e9406092837ce9822d67bf356b
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-08-28 19:36:57 +0100

    Markup: Create component for email subscribe and add content

---
+++ src/components/Subscribe.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Subscribe = () => (
+  <section>
+    <h2>35,000+ already joined</h2>
+    <p>Stay up-to-date with what we’re doing</p>
+
+    <div>
+      <input type='email' name='email' id='' />
+      <button>Contact Us</button>
+    </div>
+  </section>
+);
+
+export default Subscribe
