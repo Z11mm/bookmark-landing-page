06ce90f8c22d8a567c2a9a3d3ecdcd459f7f9abf
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-09-11 21:21:49 +0100

    Create tabs and add content

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const Tabs = () => (
   <div>
@@ -7,5 +7,30 @@ const Tabs = () => (
       <button className="tablinks">Speedy Searching</button>
       <button className="tablinks">Easy Sharing</button>
     </div>
+
+    <div id="London" class="tabcontent">
+      <h3>Bookmark in one click</h3>
+      <p>
+        Organize your bookmarks however you like. Our simple drag-and-drop
+        interface gives you complete control over how you manage your favorite
+        sites.
+      </p>
+    </div>
+    <div id="London" class="tabcontent">
+      <h3>Intelligent search</h3>
+      <p>
+        Our powerful search feature will help you find saved sites in no time at
+        all. No need to trawl through all of your bookmarks.
+      </p>
+    </div>
+    <div id="London" class="tabcontent">
+      <h3>Share your bookmarks</h3>
+      <p>
+        Easily share your bookmarks and collections with others. Create a
+        shareable link that you can send at the click of a button.
+      </p>
+    </div>
   </div>
-)
+);
+
+export default Tabs;
