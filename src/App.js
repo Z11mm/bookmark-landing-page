722a5a9e1b5a7e4338a2526a3900c5a602f0e22c
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-09-13 09:40:47 +0100

    Add FAQs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Features from './components/Features';
-import Extensions from './components/Extensions';
-import Footer from './components/Footer';
-import Subscribe from './components/Subscribe';
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Extensions from "./components/Extensions";
+import Footer from "./components/Footer";
+import Subscribe from "./components/Subscribe";
+import FAQ from "./components/FAQ";
 
-import './assets/css/main.css';
+import "./assets/css/main.css";
 
 const App = () => {
   return (
-    <main className='App'>
+    <main className="App">
       <Header />
       <Hero />
       <Features />
       <Extensions />
+      <FAQ />
       <Subscribe />
       <Footer />
     </main>
--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const FAQ = () => (
   <section>
@@ -7,10 +7,54 @@ const FAQ = () => (
       Here are some of our FAQs. If you have any other questions you’d like
       answered please feel free to email us.
     </p>
+    <div>
+      <div>
+        <button>What is Bookmark?</button>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+            tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum
+            quis quam ornare mattis.
+          </p>
+        </div>
+      </div>
+      <div>
+        <button>How can I request a new browser?</button>
+        <div>
+          <p>
+            Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa,
+            ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros
+            aliquet convallis ultricies. Mauris augue massa, ultricies non
+            ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis
+            ultricies. Mauris augue massa, ultricies non ligula. Suspendisse
+            imperdiet.
+          </p>
+        </div>
+      </div>
+      <div>
+        <button>Is there a mobile app?</button>
+        <div>
+          <p>
+            Sed consectetur quam id neque fermentum accumsan. Praesent luctus
+            vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula
+            quis est pharetra mattis sit amet pharetra purus. Sed sollicitudin
+            ex et ultricies bibendum.
+          </p>
+        </div>
+      </div>
+      <div>
+        <button>What about other Chromium browsers?</button>
+        <div>
+          <p>
+            Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat
+            mi, at euismod dui. Aliquam vitae neque eget nisl gravida
+            pellentesque non ut velit.
+          </p>
+        </div>
+      </div>
+    </div>
     <button>More Info</button>
   </section>
 );
 
 export default FAQ;
-
-qwiklabs - gcp - 01 - 03ce41b406f8: us - central1: wordpress - db
