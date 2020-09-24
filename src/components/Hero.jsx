e9a830182bfe2aa24f69746c0574d8cff49983a7
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-09-24 20:11:47 +0100

    Style: Add hero styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,16 +5,18 @@ import heroImage from '../assets/images/illustration-hero.svg';
 const Hero = () => (
   <div className='hero'>
     <section>
-      <h1>A Simple Bookmark Manager</h1>
-
-      <p>
-        A clean and simple interface to organize your favorite websites. Open a
-        new browser tab and see your sites load instantly. Try it for free.
-      </p>
-
       <div>
-        <button className='btn-chrome'>Get it on Chrome</button>
-        <button className='btn-firefox'>Get it on Firefox</button>
+        <h1>A Simple Bookmark Manager</h1>
+        
+        <p>
+          A clean and simple interface to organize your favorite websites. Open a
+          new browser tab and see your sites load instantly. Try it for free.
+        </p>
+        
+        <div className='btn-browser'>
+          <button className='btn-chrome'>Get it on Chrome</button>
+          <button className='btn-firefox'>Get it on Firefox</button>
+        </div>
       </div>
     </section>
 
