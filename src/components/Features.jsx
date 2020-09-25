a54a33820c9672e636a77fe4cc2693832d909990
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-09-25 20:06:40 +0100

    Style: Add styles to features section

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,14 +3,14 @@ import Tabs from "./Tabs";
 
 const Features = () => (
   <div>
-    <section className='features'>
+    <div className='features'>
       <h2>Features</h2>
       <p className='features-intro'>
         Our aim is to make it quick and easy for you to access your favorite
         websites. Your bookmarks sync between your devices so you can access
         them on the go.
       </p>
-    </section>
+    </div>
 
     <Tabs />
   </div>
