659aff6f97afc11c0fbeb21366870afd8214187f
Zimuzo Obiechina <zimobiechina@gmail.com>
2020-09-26 22:51:48 +0100

    Add tab content

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -8,8 +8,8 @@ const Tabs = () => (
       <button className="tablinks">Easy Sharing</button>
     </div>
 
-    <div id="London" className="tabcontent">
-      <img src="../assets/images/illustration-features-tab-1.svg" alt=""/>
+    <div id="bookmark" className="tabcontent">
+      <img src="../assets/images/illustration-features-tab-1.svg" alt="" />
       <div>
         <h3>Bookmark in one click</h3>
         <p>
@@ -20,24 +20,24 @@ const Tabs = () => (
         <button>More Info</button>
       </div>
     </div>
-    <div id="London" className="tabcontent">
-    <img src="../assets/images/illustration-features-tab-2.svg" alt=""/>
+    <div id="intellisearch" className="tabcontent">
+      <img src="../assets/images/illustration-features-tab-2.svg" alt="" />
       <div>
         <h3>Intelligent search</h3>
         <p>
-          Our powerful search feature will help you find saved sites in no time at
-          all. No need to trawl through all of your bookmarks.
+          Our powerful search feature will help you find saved sites in no time
+          at all. No need to trawl through all of your bookmarks.
         </p>
         <button>More Info</button>
       </div>
     </div>
-    <div id="London" className="tabcontent">
-    <img src="../assets/images/illustration-features-tab-3.svg" alt=""/>
+    <div id="sharebookmark" className="tabcontent">
+      <img src="../assets/images/illustration-features-tab-3.svg" alt="" />
       <div>
-        <h3>Intelligent search</h3>
+        <h3>Share your bookmarks</h3>
         <p>
-          Our powerful search feature will help you find saved sites in no time at
-          all. No need to trawl through all of your bookmarks.
+          Easily share your bookmarks and collections with others. Create a
+          shareable link that you can send at the click of a button.
         </p>
         <button>More Info</button>
       </div>
